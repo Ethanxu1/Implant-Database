1e60c09e644f8fbb23c96550abd72e30a0a982f2
Ethan Xu <ethanxu1@gmail.com>
2025-11-09 02:54:27 +0000

    Secure secret key

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from database import db, Implant, User
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key-here-change-in-production'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 db.init_app(app)
 
@@ -333,9 +335,9 @@ def init_db():
 
 if __name__ == '__main__':
     # Uncomment the line below to reset the database (will delete all data)
-    init_db()
+    # init_db()
     
     with app.app_context():
         db.create_all()
         create_default_user()
-    app.run(debug=True)
+    app.run(debug=False)
